c58602fb483b6743a9ebb57026296c8e4ecfa49c
Fernando Borges <fernandomborges96@gmail.com>
2024-03-23 14:49:02 -0300

    build(gulp): configure javascript compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,50 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const autoprefixer = require('gulp-autoprefixer');
-const browserSync = require('browser-sync').create();
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const autoprefixer = require("gulp-autoprefixer");
+const browserSync = require("browser-sync").create();
+const concat = require("gulp-concat");
 
-const SASS_PATH = 'css/scss/**/*.scss'
+const SASS_PATH = "css/scss/**/*.scss";
+const JS_PATH = "js/main/*.js";
 
-gulp.task('sass', compilaSass);
-gulp.task('watch', watch);
-gulp.task('browser-sync', browser)
-gulp.task('default', gulp.parallel(['watch', 'browser-sync']));
+gulp.task("sass", compilaSass);
+gulp.task("js", compileJs);
+gulp.task("watch", watch);
+gulp.task("browser-sync", browser);
+gulp.task("default", gulp.parallel(["watch", "browser-sync", "sass", "js"]));
 
 function compilaSass() {
   return gulp
     .src(SASS_PATH)
     .pipe(
       sass({
-        outputStyle: 'compressed',
-      }),
+        outputStyle: "compressed",
+      })
     )
     .pipe(
       autoprefixer({
         cascade: false,
-      }),
+      })
     )
-    .pipe(gulp.dest('css/'))
-    .pipe(browserSync.stream())
+    .pipe(gulp.dest("css/"))
+    .pipe(browserSync.stream());
 }
 
+function compileJs() {
+  return gulp.src(JS_PATH).pipe(concat("main.js")).pipe(gulp.dest("js/")).pipe(browserSync.stream());
+}
 
 function watch() {
   gulp.watch(SASS_PATH, compilaSass);
-  gulp.watch(['*.html']).on('change', browserSync.reload)
+  gulp.watch(JS_PATH, compileJs);
+  gulp.watch(["*.html"]).on("change", browserSync.reload);
 }
 
 function browser() {
   browserSync.init({
     port: 3002,
     server: {
-      baseDir: './',
-    }
-  })
+      baseDir: "./",
+    },
+  });
 }
